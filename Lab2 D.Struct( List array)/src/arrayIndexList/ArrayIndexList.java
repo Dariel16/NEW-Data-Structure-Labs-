fc59ef8c330d6445c00d8780e8138503a1e25a1d
DarielTemp <dariel.ramos@upr.edu>
2018-03-06 13:10:21 -0400

    labs

---
+++ Lab2 D.Struct( List array)/src/arrayIndexList/ArrayIndexList.java
@@ -0,0 +1,160 @@
+package arrayIndexList;
+
+import java.awt.List;
+import java.lang.reflect.Array;
+
+import indexList.IndexList;
+
+public class ArrayIndexList<E> implements IndexList<E> {
+	private static final int INITCAP = 1; 
+	private static final int CAPTOAR = 1; 
+	private static final int MAXEMPTYPOS = 2; 
+	private E[] element; 
+	private int size; 
+
+	public ArrayIndexList() { 
+		element = (E[]) new Object[INITCAP]; 
+		size = 0; 
+	} 
+
+
+	public void add(int index, E e) throws IndexOutOfBoundsException {
+
+		if(index > size()||index<0){
+			throw new IndexOutOfBoundsException("Invalid index = "+index);
+		}
+		if(size()==element.length){
+			changeCapacity(CAPTOAR);
+		}
+		moveDataOnePositionTR(index, size()-1);
+		element[index]=e;
+		size++;
+
+	}
+
+
+
+	public void add(E e) {
+		if((size==element.length)){
+			changeCapacity(CAPTOAR);
+
+		}							
+		element[size()]=e;	
+		size++;	
+	}
+
+
+	public E get(int index) throws IndexOutOfBoundsException {	
+		if(index> size()-1||index<0 ){
+			throw new IndexOutOfBoundsException("Invalid index = "+index);
+		}	
+
+		return element[index]; 
+	}
+
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+
+	public E remove(int index) throws IndexOutOfBoundsException {
+
+		if(index>size()-1|| index<0){
+			throw new IndexOutOfBoundsException("Invalid index = "+index);
+		}
+		E oldElem= element[index];
+
+		moveDataOnePositionTL(index+1, size-1);
+
+		if((element.length - size())> 10)
+			changeCapacity(-CAPTOAR);	
+		size--;
+
+		return oldElem;
+	}
+
+
+	public E set(int index, E e) throws IndexOutOfBoundsException {
+
+		if(index>size()-1|| index < 0){
+			throw new IndexOutOfBoundsException("Invalid index = "+index);
+		}
+		E oldElem=element[index];
+		element[index]=e;
+
+		return oldElem;
+	}
+
+
+	public int size() {
+		return size;
+	}	
+
+	public int capacity() { 
+		return element.length; 
+	}
+
+
+
+	// private methods  -- YOU CAN NOT MODIFY ANY OF THE FOLLOWING
+	// ... ANALYZE AND USE WHEN NEEDED
+
+	// you should be able to decide when and how to use
+	// following method.... BUT NEED TO USE THEM WHENEVER
+	// NEEDED ---- THIS WILL BE TAKEN INTO CONSIDERATION WHEN GRADING
+
+	private void changeCapacity(int change) { 
+		int newCapacity = element.length + change; 
+		E[] newElement = (E[]) new Object[newCapacity]; 
+		for (int i=0; i<size; i++) { 
+			newElement[i] = element[i]; 
+			element[i] = null; 
+		} 
+		element = newElement; 
+	}
+
+	// useful when adding a new element with the add
+	// with two parameters....
+	private void moveDataOnePositionTR(int low, int sup) { 
+		// pre: 0 <= low <= sup < (element.length - 1)
+		for (int pos = sup; pos >= low; pos--)
+			element[pos+1] = element[pos]; 
+	}
+
+	// useful when removing an element from the list...
+	private void moveDataOnePositionTL(int low, int sup) { 
+		// pre: 0 < low <= sup <= (element.length - 1)
+		for (int pos = low; pos <= sup; pos++)
+			element[pos-1] = element[pos]; 
+	}
+
+
+	// The following two methods are to be implemented as part of an exercise
+	public Object[] toArray() {
+
+		Object[] s= new Object[element.length];
+		for(int index=0; index< element.length;index++){
+			s[index]= element[index];
+		}
+		return s;
+	}
+
+
+	@Override
+	public <T1> T1[] toArray(T1[] array) {
+		if (array.length<this.size()) {
+			array = (T1[]) Array.newInstance(array.getClass().getComponentType(),this.size());
+		}
+		else if(array.length> this.size()){
+			for (int index = 0; index < array.length; index++) {
+				array[index] = null;
+				for (int j = 0; j < this.size(); j++) {
+					array[j] = (T1) this.get(j);
+				}
+			}
+		}
+		return array;
+	}
+
+}
